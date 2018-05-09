01c26ebf59807d416744325b5b2a0766f6538b26
barry1101 <q369140975@outlook.com>
2018-05-09 16:27:01 +0800

    添加项目文件。

---
+++ MyControlLibrary/LigatureControl.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyControlLibrary
+{
+    /// <summary>
+    /// 单词连线控件
+    /// </summary>
+    public partial class LigatureControl: UserControl
+    {
+        public LigatureControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ OCRDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace OCRDemo
+{
+    static class Program
+    {
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern bool SetProcessDPIAware();
+
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            //高DPI支持
+            if (Environment.OSVersion.Version.Major >= 6) { SetProcessDPIAware(); }
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainForm());
+        }
+    }
+}
